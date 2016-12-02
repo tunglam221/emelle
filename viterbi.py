ab34ed479dccb07c215e8a3660191d8f8c9d8848
Lam <tunglam221@gmail.com>
2016-12-02 21:48:50 +0800

    some changes to viterbi, untested

--- viterbi.py
+++ viterbi.py
@@ -2,63 +2,89 @@ import math
 from state_num_dictionary import *
 
 class Node:
-        def __init__(self, score, parent, state):
-                self.score = score
-                self.parent = parent
-                self.state = state
+	def __init__(self, score, parent, state):
+		self.score = score
+		self.parent = parent
+		self.state = state
 
 class Viterbi:
-        def __init__(self, tran, emis):
-                self.tran = tran
-                self.emis = emis
-                self.number_of_states = len(tran)
+	def __init__(self, tran, emis, entity_emis):
+		self.tran = tran
+		self.emis = emis
+		self.entity_emis = entity_emis
+		self.number_of_states = len(tran)
 
-        def max_node(self, graph, i, j, x):
-                max_score = -float('inf')
-                parent = None
-                if i > len(x):  # STOP state
-                        emis = 1
-                elif x[i-1] not in self.emis:
-                        emis = self.emis[j]
-                else:
-                        emis = self.emis[x[i-1]][j]
-                for node in graph[i-1]:
-                        score = node.score + log(self.tran[node.state][j]) + log(emis)
-                        if max_score <= score:
-                                max_score = score
-                                parent = node
-                return Node(max_score, parent, j)
+	def max_node(self, graph, i, j, x, pretag, entity=''):
+		max_score = -float('inf')
+		
+		parent = None
+		if i > len(x):  # STOP state
+			emis = 1
+		elif pretag == 'O' and j!=1:
+			emis = 0
+		elif pretag == 'B':
+			if j not in [2, 3, 4]:
+				emis = 0
+			else:
+				emis = self.entity_emis[]
+		elif pretag == 'I':
+			if j not in [5, 6, 7]:
+				emis = 0
+			else:
+				parent = graph[i-1][j-3]
+				return Node(parent.score, parent, j)
+		elif x[i-1] not in self.emis:
+			emis = self.emis[j]
+		else:
+			emis = self.emis[x[i-1]][j]
 
-        def decode(self, sentence):
-                x = sentence.observation
-                n = len(x)
-                t = self.number_of_states
-                graph = []
+		for node in graph[i-1]:
+			score = node.score + log(self.tran[node.state][j]) + log(emis)
+			if max_score <= score:
+				max_score = score
+				parent = node
+		return Node(max_score, parent, j)
 
-                # set node(0,0)
-                graph.append([])
-                graph[0].append(Node(0, None, 0))
+	def decode(self, sentence):
+		x = sentence.observation
+		entities = sentence.findEntity()
+		n = len(x)
+		t = self.number_of_states
+		graph = []
 
-                for i in range(1, n+1): #step i
-                        graph.append([])
-                        for j in range(1, t-1):
-                                graph[i].append(self.max_node(graph, i, j, x))
+		# set node(0,0)
+		graph.append([])
+		graph[0].append(Node(0, None, 0))
 
-                # step n+1
-                graph.append([])
-                graph[n+1].append(self.max_node(graph, n+1, t-1, x))
+		for i in range(1, n+1): #step i
+			graph.append([])
+			if entities[i-1]=='B':
+				e = x[i-1]
+				j = i
+				while entities[j]=='I':
+					e = e + ' ' + x[j]
+					j+=1
+				for j in range(1, t-1):
+					graph[i].append(self.max_node(graph, i, j, x, 'B', e))
+			else:
+				for j in range(1, t-1):
+					graph[i].append(self.max_node(graph, i, j, x, entities[i-1]))
 
-                path = []
-                next = graph[n+1][0]
-                for i in range(n+1, 1, -1):
-                        path.insert(0, next.parent.state)
-                        next = next.parent
+		# step n+1
+		graph.append([])
+		graph[n+1].append(self.max_node(graph, n+1, t-1, x))
 
-                for p in path:
-                        sentence.state.append(num2state[p])
+		path = []
+		next = graph[n+1][0]
+		for i in range(n+1, 1, -1):
+			path.insert(0, next.parent.state)
+			next = next.parent
+
+		for p in path:
+			sentence.state.append(num2state[p])
 
 def log(a):
-        if a==0:
-                return -float('inf')
-        else:
-                return math.log(a)
+	if a==0:
+		return -float('inf')
+	else:
+		return math.log(a)
